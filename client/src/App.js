304cb13d3adca3039f59ec3967ff8ced54d7cdbb
Daniel <danebraaksma@hotmail.com>
2022-07-12 19:41:13 -0400

    added initial routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./components/Home"
+import CastVote from "./components/CastVote"
 
 function App() {
   return (
     <div>
       <Navbar />
       <Routes>
-        <Route exact path="/" element={} />
-        <Route path="/" element={} />
+        <Route exact path="/" element={<Home />} />
+        <Route path="/votes" element={<CastVote />} />
+
       </Routes>
     </div>
   );
