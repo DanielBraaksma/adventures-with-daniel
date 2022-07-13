81a3be1dc1bc8cff253a273b03b7322b2af8d371
Daniel <danebraaksma@hotmail.com>
2022-07-12 22:14:09 -0400

    starting to work on homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/Navbar"
 import Home from "./components/Home"
 import CastVote from "./components/CastVote"
 
@@ -8,11 +9,7 @@ function App() {
   return (
     <div>
       <Navbar />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/votes" element={<CastVote />} />
-
-      </Routes>
+      
     </div>
   );
 }
---
+++ client/src/components/Home.js
@@ -0,0 +1,6 @@
+import React from "react"
+// https://codesandbox.io/s/ll9r7wxkmq?file=/src/index.js:0-555 great resource for making map
+
+export default function Home () {
+
+}
