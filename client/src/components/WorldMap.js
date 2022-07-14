bbbe2c797ace3c7c0d184bf4b63dee4fd979b0bd
Daniel <danebraaksma@hotmail.com>
2022-07-13 22:55:07 -0400

    changing map options

--- client/src/components/WorldMap.js
+++ client/src/components/WorldMap.js
@@ -4,26 +4,31 @@ import Chart from "react-google-charts";
 export default function WorldMap (props) {
     const data = [
         ["Country", "Date Visited"],
-        ["Kenya", 200],
-        ["United States", 300],
-        ["Uganda", 400],
+        ["Kenya", 500],
+        ["United States", 500],
+        ["Uganda", 500],
         ["Vietnam", 500],
         ["Thailand", 500],
         ["Laos", 500],
         ["Cambodia", 500],
         ["Malaysia", 500],
-        ["Philippines", 600],
-        ["Philippines", 600],
-        ["Indonesia", 600],
-        ["Tanzania", 700],
-        ["Netherlands", 700],
-        ["South-Sudan", 700],
-        ["Germany", 700]
+        ["Philippines", 500],
+        ["Philippines", 500],
+        ["Indonesia", 500],
+        ["Tanzania", 500],
+        ["Netherlands", 500],
+        ["South-Sudan", 500],
+        ["Germany", 500]
       ];
 
+    const options = {
+        backgroundColor: "lightblue",
+        legend: "none",
+    }
+
     return (
-        <div className="App">
-        <Chart chartType="GeoChart" width="100%" height="800px" data={data} />
+        <div className="map">
+        <Chart chartType="GeoChart" max-width="100%" height="400px" data={data} options={options} borderRadius="20px"/>
       </div>
     )
 }
