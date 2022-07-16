1561f47c4b1f66ac48947a95467e099c7f1f4e4d
Daniel <danebraaksma@hotmail.com>
2022-07-16 14:52:02 -0400

    changed WorldMap to class based component per documentation of svgmap

--- client/src/components/WorldMap.js
+++ client/src/components/WorldMap.js
@@ -1,79 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import svgMap from 'svgmap'
 import 'svgmap/dist/svgMap.min.css';
 
+class App extends Component {
 
-export default function WorldMap (props) {
+  componentDidMount() {
+    if (!this.svgMap) {
 
-  new svgMap({
-    targetElementID: 'svgMap',
-    colorMin: '#85ffbd',
-    colorMax: '#85ffbd',
-    noDataText: "Hope to visit someday!",
-    data: {
-      data: {
-        yearVisited: {
-          name: 'year visited',
-          format: '',
-          thresholdMax: 5000,
-          thresholdMin: 0
-        },
-        lived: {
-          name: 'lived',
-          format: ''
-        },
-        stayLength: {
-          name: "stay length",
-          format: ''
+      console.log(this.state);
+
+      var mySvgMap = new svgMap({
+        targetElementID: 'svgMap',
+        colorMin: '#85ffbd',
+        colorMax: '#85ffbd',
+        noDataText: "Hope to visit someday!",
+        data: {
+          data: {
+            yearVisited: {
+              name: 'year visited',
+              format: '',
+              thresholdMax: 5000,
+              thresholdMin: 0
+            },
+            lived: {
+              name: 'lived',
+              format: ''
+            },
+            stayLength: {
+              name: "stay length",
+              format: ''
+            }
+          },
+          applyData: 'yearVisited',
+          values: {
+            AF: {yearVisited: 1997, lived: "true", stayLength: "1 week"},
+            BR: {yearVisited: 2010, lived: "true", stayLength: "5 weeks"},
+            AU: {yearVisited: 2022, lived: "false", stayLength: "currently living here"}
+            // ...
+          },
         }
-      },
-      applyData: 'yearVisited',
-      values: {
-        AF: {yearVisited: 1997, lived: "true", stayLength: "1 week"},
-        BR: {yearVisited: 2010, lived: "true", stayLength: "5 weeks"},
-        AU: {yearVisited: 2022, lived: "false", stayLength: "currently living here"}
-        // ...
-      },
+      });
+      this.svgMap = mySvgMap;
     }
-  });
+  }
 
-
-
-
-
-  // new svgMap({
-  //   targetElementID: 'svgMap',
-  //   colorMin: '#85ffbd',
-  //   colorMax: '#85ffbd',
-  //   noDataText: "Hope to visit someday!",
-  //   data: {
-  //     data: {
-  //       yearVisited: {
-  //         name: 'year visited',
-  //         format: '',
-  //         thresholdMax: 5000,
-  //         thresholdMin: 0
-  //       },
-  //       lived: {
-  //         name: 'lived',
-  //         format: ''
-  //       },
-  //       stayLength: {
-  //         name: "stay length",
-  //         format: ''
-  //       }
-  //     },
-  //     applyData: 'yearVisited',
-  //     values: {
-  //       AF: {yearVisited: 1997, lived: "true", stayLength: "1 week"},
-  //       BR: {yearVisited: 2010, lived: "true", stayLength: "5 weeks"},
-  //       AU: {yearVisited: 2022, lived: "false", stayLength: "currently living here"}
-  //       // ...
-  //     },
-  //   }
-  // });
-
+  render() {
     return (
-      <div id="svgMap">test</div>
-    )
+        <div id='svgMap'></div>
+    );
+  }
 }
+
+export default App;
